8a25355a67b26152a50dee4b7448b6a79f15496e
max <mcnic@users.noreply.github.com>
2024-03-31 20:12:43 -0700

    feat: init

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return <>app</>;
+}
+
+export default App;
---
+++ src/const/technologies.ts
@@ -0,0 +1,181 @@
+type Tech = {
+  name: string;
+  url: string;
+  img: string;
+};
+
+export const Technologies: Tech[] = [
+  {
+    name: 'js',
+    url: '',
+    img: 'https://skillicons.dev/icons?i=js',
+  },
+  {
+    name: 'ts',
+    url: '',
+    img: 'https://skillicons.dev/icons?i=ts',
+  },
+  {
+    name: 'git',
+    url: '',
+    img: 'https://skillicons.dev/icons?i=git',
+  },
+  {
+    name: 'html',
+    url: '',
+    img: 'https://skillicons.dev/icons?i=html',
+  },
+  {
+    name: 'css',
+    url: '',
+    img: 'https://skillicons.dev/icons?i=css',
+  },
+  {
+    name: 'scss',
+    url: '',
+    img: 'https://skillicons.dev/icons?i=scss',
+  },
+  {
+    name: 'mui',
+    url: '',
+    img: 'https://skillicons.dev/icons?i=mui',
+  },
+  {
+    name: 'tailwind',
+    url: '',
+    img: 'https://skillicons.dev/icons?i=tailwind',
+  },
+  {
+    name: 'webpack',
+    url: '',
+    img: 'https://skillicons.dev/icons?i=webpack',
+  },
+  {
+    name: 'vite',
+    url: '',
+    img: '"https://skillicons.dev/icons?i=vite',
+  },
+  {
+    name: 'react',
+    url: '',
+    img: 'https://skillicons.dev/icons?i=react',
+  },
+  {
+    name: 'redux',
+    url: '',
+    img: 'https://skillicons.dev/icons?i=redux',
+  },
+  {
+    name: 'nextjs',
+    url: '',
+    img: 'https://skillicons.dev/icons?i=nextjs',
+  },
+  {
+    name: 'angular',
+    url: '',
+    img: 'https://skillicons.dev/icons?i=angular',
+  },
+  {
+    name: 'nodejs',
+    url: '',
+    img: 'https://skillicons.dev/icons?i=nodejs',
+  },
+  {
+    name: 'express',
+    url: '',
+    img: 'https://skillicons.dev/icons?i=express',
+  },
+  {
+    name: 'nest',
+    url: '',
+    img: 'https://skillicons.dev/icons?i=nest',
+  },
+  {
+    name: 'graphql',
+    url: '',
+    img: 'https://skillicons.dev/icons?i=graphql',
+  },
+  {
+    name: 'jest',
+    url: '',
+    img: 'https://skillicons.dev/icons?i=jest',
+  },
+  {
+    name: 'vscode',
+    url: '',
+    img: 'https://skillicons.dev/icons?i=vscode',
+  },
+  {
+    name: 'webstorm',
+    url: '',
+    img: 'https://skillicons.dev/icons?i=webstorm',
+  },
+  {
+    name: 'linux',
+    url: '',
+    img: 'https://skillicons.dev/icons?i=linux',
+  },
+];
+
+/*
+        <a href="https://reactjs.org/" target="_blank" rel="noreferrer"> 
+        <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" alt="react" width="40" height="40"/> </a> 
+        
+        <a href="https://redux.js.org" target="_blank" rel="noreferrer"> 
+        <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg" alt="redux" width="40" height="40"/>
+         </a>
+         
+         <a href="https://sass-lang.com" target="_blank" rel="noreferrer"> 
+         <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg" alt="sass" width="40" height="40"/> </a>
+         
+         <a href="https://tailwindcss.com/" target="_blank" rel="noreferrer"> 
+         <img src="https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg" alt="tailwind" width="40" height="40"/> </a> 
+         
+         <a href="https://www.typescriptlang.org/" target="_blank" rel="noreferrer">
+          <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg" alt="typescript" width="40" height="40"/> </a> 
+          
+          <a href="https://webpack.js.org" target="_blank" rel="noreferrer"> 
+          <img src="https://raw.githubusercontent.com/devicons/devicon/d00d0969292a6569d45b06d3f350f463a0107b0d/icons/webpack/webpack-original-wordmark.svg" alt="webpack" width="40" height="40"/> </a> 
+          
+          <a href="https://angular.io" target="_blank" rel="noreferrer"> 
+          <img src="https://angular.io/assets/images/logos/angular/angular.svg" alt="angular" width="40" height="40"/> </a>
+          
+          <a href="https://babeljs.io/" target="_blank" rel="noreferrer"> 
+          <img src="https://www.vectorlogo.zone/logos/babeljs/babeljs-icon.svg" alt="babel" width="40" height="40"/> </a>
+          
+          <a href="https://getbootstrap.com" target="_blank" rel="noreferrer"> 
+          <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg" alt="bootstrap" width="40" height="40"/> </a>
+          
+          <a href="https://www.w3schools.com/css/" target="_blank" rel="noreferrer"> 
+          <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width="40" height="40"/> </a>
+          
+          <a href="https://www.cypress.io" target="_blank" rel="noreferrer">
+           <img src="https://raw.githubusercontent.com/simple-icons/simple-icons/6e46ec1fc23b60c8fd0d2f2ff46db82e16dbd75f/icons/cypress.svg" alt="cypress" width="40" height="40"/> </a> 
+           
+           <a href="https://www.figma.com/" target="_blank" rel="noreferrer"> 
+           <img src="https://www.vectorlogo.zone/logos/figma/figma-icon.svg" alt="figma" width="40" height="40"/> </a>
+           
+           <a href="https://firebase.google.com/" target="_blank" rel="noreferrer"> 
+           <img src="https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg" alt="firebase" width="40" height="40"/> </a>
+           
+           <a href="https://git-scm.com/" target="_blank" rel="noreferrer"> 
+           <img src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg" alt="git" width="40" height="40"/> </a>
+           
+           <a href="https://graphql.org" target="_blank" rel="noreferrer">
+            <img src="https://www.vectorlogo.zone/logos/graphql/graphql-icon.svg" alt="graphql" width="40" height="40"/> </a>
+            
+            <a href="https://www.w3.org/html/" target="_blank" rel="noreferrer"> 
+            <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width="40" height="40"/> </a>
+            
+            <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer"> 
+            <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="40" height="40"/> </a> 
+            
+            <a href="https://jestjs.io" target="_blank" rel="noreferrer"> 
+            <img src="https://www.vectorlogo.zone/logos/jestjsio/jestjsio-icon.svg" alt="jest" width="40" height="40"/> </a>
+             
+            <a href="https://nextjs.org/" target="_blank" rel="noreferrer"> 
+             <img src="https://cdn.worldvectorlogo.com/logos/nextjs-2.svg" alt="nextjs" width="40" height="40"/> </a>
+             
+             <a href="https://nodejs.org" target="_blank" rel="noreferrer">
+              <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" alt="nodejs" width="40" height="40"/> </a>
+              */
